f6125fa08583cd85bd4a50ceeb8a2972ac6138d7
Rian-Sanjaya <rian.sj@gmail.com>
2018-01-14 16:20:14 +0700

    Add event validation and rescue method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,11 @@
 class EventsController < ApplicationController
+	before_action :set_event, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@events = Event.all
 	end
 
 	def show
-		@event = Event.find(params[:id])
 	end
 
 	def new
@@ -12,7 +13,6 @@ class EventsController < ApplicationController
 	end
 
 	def create
-		@event = Event.new(event_params)
 		if @event.save
 			flash[:notice] = "Event created!"
 			redirect_to @event
@@ -27,7 +27,6 @@ class EventsController < ApplicationController
 	end
 
 	def update
-		@event = Event.find(params[:id])
 		if @event.update(event_params)
 			flash[:notice] = "Event updated!"
 			redirect_to @event
@@ -38,13 +37,20 @@ class EventsController < ApplicationController
 	end
 
 	def destroy
-		@event = Event.find(params[:id])
 		@event.destroy
 		flash[:alert] = "Event delete successfully."
 		redirect_to events_url
 	end
 
 	private
+
+		def set_event
+			@event = Event.find(params[:id])
+
+		rescue ActiveRecord::RecordNotFound
+			flash[:alert] = "The page you just requested is not exist."
+			redirect_to events_path
+		end
 
 		def event_params
 			params.require(:event).permit(:title, :description, :start_date, :end_date, :venue, :location)

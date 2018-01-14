c845bb8f3049913f08d945233035f90c92086a75
Rian-Sanjaya <rian.sj@gmail.com>
2018-01-14 11:58:55 +0700

    Add destroy action to event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,12 @@
 class EventsController < ApplicationController
+	def index
+		@events = Event.all
+	end
+
+	def show
+		@event = Event.find(params[:id])
+	end
+
 	def new
 		@event = Event.new
 	end
@@ -14,9 +22,31 @@ class EventsController < ApplicationController
 		end
 	end
 
+	def edit
+		@event = Event.find(params[:id])
+	end
+
+	def update
+		@event = Event.find(params[:id])
+		if @event.update(event_params)
+			flash[:notice] = "Event updated!"
+			redirect_to @event
+		else
+			flash[:alert] = "Event not updated"
+			render "edit"
+		end
+	end
+
+	def destroy
+		@event = Event.find(params[:id])
+		@event.destroy
+		flash[:alert] = "Event delete successfully."
+		redirect_to events_url
+	end
+
 	private
 
 		def event_params
-			params.require(:event).permit(:title, :description, :start_date, :end_date, :venue, :lobation)
+			params.require(:event).permit(:title, :description, :start_date, :end_date, :venue, :location)
 		end
 end

c300d6e29a6cfb4ef5f5901372d5da4765bff43b
Rian <rian.sj@gmail.com>
2018-01-23 12:04:19 +0000

    Add assotiation and authorization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180122110814) do
+ActiveRecord::Schema.define(version: 20180123105439) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -22,7 +22,10 @@ ActiveRecord::Schema.define(version: 20180122110814) do
     t.string   "location"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "events", ["user_id"], name: "index_events_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
